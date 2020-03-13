440aecba5d351c84c0ab9a398d1aaf04e85bc79d
blakelower <blakelower1999@gmail.com>
2020-03-13 15:21:09 +0700

    home page is *almost* finished

--- fx/src/components/home.js
+++ fx/src/components/home.js
@@ -2,15 +2,17 @@ import React from "react";
 import "../styles/fx.css";
 import "../styles/navbar.scss";
 import 'bootstrap/dist/css/bootstrap.css'
+import Girl from "../img/01.jpg";
+import RockBoy from "../img/02.jpg";
+import Star from "../img/03.jpg";
+import Logo from "../img/logo.png";
 
 export default function Home() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
         <div className="container">
-          <a className="navbar-brand" href="#">
-            Start Bootstrap
-          </a>
+          <img className="logo" src={Logo} alt="logo"/>          
           <button
             className="navbar-toggler"
             type="button"
@@ -55,15 +57,15 @@ export default function Home() {
         <div className="bg-circle-3 bg-circle"></div>
         <div className="bg-circle-4 bg-circle"></div>
       </header>
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-lg-6 order-lg-2">
-            <div class="p-5">
-              <img class="img-fluid rounded-circle" src="img/01.jpg" alt="" />
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-6 order-lg-2">
+            <div className="p-5">
+              <img className="img-fluid rounded-circle" src={RockBoy}/>
             </div>
-            <div class="col-lg-6 order-lg-1">
-              <div class="p-5">
-                <h2 class="display-4">For those about to rock...</h2>
+            <div className="col-lg-6 order-lg-1">
+              <div className="p-5">
+                <h2 className="display-4">For those about to rock...</h2>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
                   aliquid, mollitia odio veniam sit iste esse assumenda amet
@@ -75,15 +77,15 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-lg-6">
-            <div class="p-5">
-              <img class="img-fluid rounded-circle" src="img/02.jpg" alt="" />
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-6">
+            <div className="p-5">
+              <img className="img-fluid rounded-circle" style={{float: "left" }} src={Girl} alt="Girl" />
             </div>
-            <div class="col-lg-6">
-              <div class="p-5">
-                <h2 class="display-4">We salute you</h2>
+            <div className="col-lg-6">
+              <div className="p-5">
+                <h2 className="display-4">We salute you</h2>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
                   aliquid, mollitia odio veniam sit iste esse assumenda amet
@@ -95,15 +97,15 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-lg-6 order-lg-2">
-            <div class="p-5">
-              <img class="img-fluid rounded-circle" src="img/03.jpg" alt="" />
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-6 order-lg-2">
+            <div className="p-5">
+              <img className="img-fluid rounded-circle" src={Star} alt="" />
             </div>
-            <div class="col-lg-6 order-lg-1">
-              <div class="p-5">
-                <h2 class="display-4">Let there be rock</h2>
+            <div className="col-lg-6 order-lg-1">
+              <div className="p-5">
+                <h2 className="display-4">Let there be rock</h2>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
                   aliquid, mollitia odio veniam sit iste esse assumenda amet
@@ -115,9 +117,9 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <footer class="py-5 bg-black">
-        <div class="container">
-          <p class="m-0 text-center text-white small">
+      <footer className="py-5 bg-black">
+        <div className="container">
+          <p className="m-0 text-center text-white small">
             Copyright &copy; Your Website 2019
           </p>
         </div>
